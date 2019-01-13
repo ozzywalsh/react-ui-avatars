2d0f2f8a6c1e0d9a9c4c5a01f118e1bea8730d54
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-13 23:21:44 +0000

    Configure babel-eslint

--- src/index.js
+++ src/index.js
@@ -1,50 +1,49 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import _ from 'lodash';
 import qs from 'qs';
 
 class UIAvatar extends Component {
-	/* Global settings object
-	 * which can be set by the user of the component
-	 * This can be overriden by passing props to individual instances
-	 */
-	static settings = {}
+  /* Global settings object
+   * which can be set by the user of the component
+   * This can be overriden by passing props to individual instances
+   */
+  static settings = {}
 
-	static APIOptions = [
-		'name',
-		'size',
-		'font-size',
-		'length',
-		'rounded',
-		'background',
-		'color',
-		'uppercase',
-	]
+  static APIOptions = [
+    'name',
+    'size',
+    'font-size',
+    'length',
+    'rounded',
+    'background',
+    'color',
+    'uppercase'
+  ]
 
+  getURL (settings) {
+    const API_URL = 'https://ui-avatars.com/api/';
+    const query = qs.stringify({ ...UIAvatar.settings, ...settings });
 
-	getURL (settings) {
-		const API_URL = 'https://ui-avatars.com/api/'
-		const query = qs.stringify({...UIAvatar.settings, ...settings});
+    const imageURL = `${API_URL}?${query}`;
 
-		const imageURL = `${API_URL}?${query}`
+    return imageURL;
+  }
 
-		return imageURL;
-	}
+  getSettings (props) {
+    return _.pickBy(props, (value, key) => _.includes(UIAvatar.APIOptions, key));
+  }
 
-	getSettings (props) {
-		return _.pickBy(props, (value, key) => _.includes(UIAvatar.APIOptions, key));
-	}
+  getExtraProps (props) {
+    return _.pickBy(props, (value, key) => !(_.includes(UIAvatar.APIOptions, key)));
+  }
 
-	getExtraProps (props) {
-		return _.pickBy(props, (value, key) => !(_.includes(UIAvatar.APIOptions, key)));
-	}
+  render () {
+    const imageURL = this.getURL(this.props);
+    const extraProps = this.getExtraProps(this.props);
 
-  render() {
-		const imageURL = this.getURL(this.props);
-		const extraProps = this.getExtraProps(this.props);
-
-		return (
-			<img src={imageURL} alt={this.props.name} {...extraProps} />
-		);
+    return (
+      <img src={imageURL} alt={this.props.name} {...extraProps} />
+    );
   }
 }
 
