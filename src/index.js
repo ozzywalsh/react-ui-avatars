5305525ccc844f42be03ac502d79b40c8747ddec
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-13 23:46:03 +0000

    Remove dependence on lodash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import qs from 'qs';
 
 class UIAvatar extends Component {
@@ -17,17 +16,6 @@ class UIAvatar extends Component {
   static settings = {}
   static defaultProps = this.settings;
 
-  static APIOptions = [
-    'name',
-    'size',
-    'font-size',
-    'length',
-    'rounded',
-    'background',
-    'color',
-    'uppercase'
-  ]
-
   getURL (settings) {
     const API_URL = 'https://ui-avatars.com/api/';
     const query = qs.stringify({ ...UIAvatar.settings, ...settings });
@@ -35,14 +23,6 @@ class UIAvatar extends Component {
     const imageURL = `${API_URL}?${query}`;
 
     return imageURL;
-  }
-
-  getSettings (props) {
-    return _.pickBy(props, (value, key) => _.includes(UIAvatar.APIOptions, key));
-  }
-
-  getExtraProps (props) {
-    return _.pickBy(props, (value, key) => !(_.includes(UIAvatar.APIOptions, key)));
   }
 
   render () {
