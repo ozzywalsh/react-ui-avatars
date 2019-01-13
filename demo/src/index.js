ed86df9dd4f1bee4c47e5d15dce950d90bb693ef
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-13 23:09:08 +0000

    Format code

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import UIAvatar from '../../src'
+import UIAvatar from '../../src';
 
 UIAvatar.settings.rounded = true;
 
 class Demo extends Component {
-  render() {
-		return (
-			<div>
-				<h1>react-ui-avatars Demo</h1>
-				<UIAvatar 
-					name='Mary Burke'
-					color='#FF69B4'
-				/>
-			</div>
-		);
+  render () {
+    return (
+      <div>
+        <h1>react-ui-avatars Demo</h1>
+        <UIAvatar
+          name='Mary Burke'
+          color='#FF69B4'
+        />
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'));
